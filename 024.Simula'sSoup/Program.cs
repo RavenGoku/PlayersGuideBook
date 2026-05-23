0b360c4138380f1cab51acf173977799898c81d6
Kamil Mikolajewski <raven.goku@gmail.com>
2026-05-23 12:14:13 +0100

    finish my code for tuples

--- 024.Simula'sSoup/Program.cs
+++ 024.Simula'sSoup/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*Challenge --------------------- Simula’s Soup------------------------------------------------------------ 100 XP
- 
+
 Simula is impressed with how you reconstructed the box with an enumeration. When the box opened,
 you saw a glowing emerald gem inside. You don’t know what it is, but it seems important. Also in the box
 were three vials of powder labeled HOT, SALTY, and SWEET.
@@ -25,21 +25,20 @@ text input against specific strings to determine which enumeration value represe
 Hint: You don’t need to convert the enumeration value back to a string. Simply displaying an
 enumeration value with Write or WriteLine will display the name of the enumeration value.) */
 
+using System.ComponentModel.Design;
+using System.Drawing;
+
 Console.Title = "Simula's Soup";
-var dish = (Recipe: Recipe.Stew, Ingredients: Ingredients.Carrots, Seasoning: Seasoning.Spicy);
-
 DisplayMenu();
+Recipe chosen_recipe = GetRecipe();
+Ingredients chosen_ingredient = GetIngredient();
+Seasoning chosen_seasoning = GetSeasoning();
 
-Console.WriteLine("User, what dish would you like to get?");
-while (true) { 
-    Console.Write("Recipe: ");
-    int choose = Convert.ToInt32(Console.ReadKey());
-    if 
-}
+var dish = (Recipe: chosen_recipe, Ingredient: chosen_ingredient, Seasoning: chosen_seasoning);
 
-
-
-
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"You have chosen: {dish.Seasoning} {dish.Ingredient} {dish.Recipe}");
+Console.ResetColor();
 
 
 
@@ -49,22 +48,141 @@ void DisplayMenu()
 {
     Console.Write(
         "===============  Menu ==================\n" +
-        "Recipe: ==============\n" +
-        "1.Soup\n" +
-        "2.Stew\n" +
-        "3.Gumbo\n" +
-        "Ingredient: ========\n" +
-        "1.Mushrooms\n" +
-        "2.Chicken\n" +
-        "3.Carrots\n" +
-        "4.Potatoes\n" +
-        "Seasoning: ==========\n" +
-        "1.Spicy\n" +
-        "2.Sweet\n" +
-        "3.Salty\n" +
-        "=======================================");
+        "  ============= Recipe: ==============\n" +
+        "   1.Soup\n" +
+        "   2.Gumbo\n" +
+        "   3.Stew\n" +
+        "  ============ Ingredient: ===========\n" +
+        "   1.Mushrooms\n" +
+        "   2.Chicken\n" +
+        "   3.Carrots\n" +
+        "   4.Potatoes\n" +
+        "  ============ Seasoning: ============\n" +
+        "   1.Spicy\n" +
+        "   2.Sweet\n" +
+        "   3.Salty\n" +
+        "=======================================\n\n");
+    Console.WriteLine("User, what dish would you like to get?");
 }
+//------------------------------------------------------------------------------------------
+Recipe GetRecipe()
+{
+    Recipe recipe;
+    int choice;
+    while (true)
+    {
+        Console.Write("Recipe: ");
+        choice = Convert.ToInt32(Console.ReadLine());
+        recipe = choice switch
+        {
+            1 => Recipe.Soup,
+            2 => Recipe.Gumbo,
+            3 => Recipe.Stew,
+            _ => Recipe.Unknown
+        };
+        if (recipe != Recipe.Unknown)
+        {
+            Console.Clear();
+            DisplayMenu();
+            
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Wrong choice, try again!\r");
+            
+        }
+    }
 
-enum Recipe {Soup = 1,Gumbo,Stew};
-enum Ingredients{Mushrooms = 1,Chicken,Carrots,Potatoes};
-enum Seasoning{Spicy = 1, Salty, Sweet};
+    return recipe;
+}
+//------------------------------------------------------------------------------------------
+Ingredients GetIngredient()
+{
+    Ingredients ingredients;
+    int choice;
+    while (true)
+    {
+        Console.Write("Ingredient: ");
+        choice = Convert.ToInt32(Console.ReadLine());
+        ingredients = choice switch
+        {
+            1 => Ingredients.Mushrooms,
+            2 => Ingredients.Chicken,
+            3 => Ingredients.Carrots,
+            4 => Ingredients.Potatoes,
+            _ => Ingredients.Unknown
+        };
+        if (ingredients != Ingredients.Unknown)
+        {
+            Console.Clear();
+            DisplayMenu();
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Wrong choice, try again!\r");
+        }
+    }
+
+    return ingredients;
+}
+//------------------------------------------------------------------------------------------
+Seasoning GetSeasoning()
+{
+    Seasoning seasoning;
+    int choice;
+    while (true)
+    {
+        Console.Write("Seasoning: ");
+        choice = Convert.ToInt32(Console.ReadLine());
+        seasoning = choice switch
+        {
+            1 => Seasoning.Spicy,
+            2 => Seasoning.Salty,
+            3 => Seasoning.Sweet,
+            _ => Seasoning.Unknown
+        };
+        if (seasoning != Seasoning.Unknown)
+        {
+            Console.Clear();
+            DisplayMenu();
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Wrong choice, try again!\r");
+          
+        }
+    }
+
+    return seasoning;
+}
+//------------------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------------------
+
+enum Recipe
+{
+    Unknown = 0,
+    Soup = 1,
+    Gumbo,
+    Stew
+};
+
+enum Ingredients
+{
+    Unknown = 0,
+    Mushrooms = 1,
+    Chicken,
+    Carrots,
+    Potatoes
+};
+
+enum Seasoning
+{
+    Unknown = 0,
+    Spicy = 1,
+    Salty,
+    Sweet
+};
